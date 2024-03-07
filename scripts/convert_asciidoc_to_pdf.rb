62b0d3079b421d9a048763dca7220522a61ac5ff
TAMAKI Junji <junji.tamaki@exmotion.co.jp>
2024-03-08 08:35:50 +0900

    test

--- scripts/convert_asciidoc_to_pdf.rb
+++ scripts/convert_asciidoc_to_pdf.rb
@@ -3,7 +3,7 @@ opt = OptionParser.new
 option = {}
 opt.on('-i VAL', 'description:input asciidoc dir') {|v| option[:input] = v }
 opt.on('-t VAL', 'description:themes dir') {|v| option[:themes] = v }
-opt.on('-f VAL', 'description:fonts dir') {|v| option[:fonts] = v }
+#opt.on('-f VAL', 'description:fonts dir') {|v| option[:fonts] = v }
 opt.on('-c VAL', 'description:converter dir') {|v| option[:converter] = v }
 opt.on('-o VAL', 'description:output pdf dir') {|v| option[:output] = v }
 opt.parse!(ARGV)
@@ -12,7 +12,7 @@ docs = Dir.glob("#{option[:input]}/**/*.adoc")
 args = [
   "-a pdf-theme=specification",
   "-a pdf-themesdir=#{option[:themes]}",
-  "-a pdf-fontsdir=#{option[:fonts]}",
+  #"-a pdf-fontsdir=#{option[:fonts]}",
   "-a scripts=cjk",
   "-r #{option[:converter]/extended-pdf-converter.rb}",
   "-D #{option[:output]}"
